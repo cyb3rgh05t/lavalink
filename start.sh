a8a5b6abaa86cb63fecb0fccbe7d70c26007fdd7
cyb3rgh05t <flammang.yves@gmail.com>
2022-08-15 17:22:42 +0200

    update

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Lavalink Bootstrapper
 
-cd /opt/Lavalink || exit
+cd "$(dirname "$0")" || exit
 
 echo "Downloading lavalink jar..."
 curl -s https://api.github.com/repos/freyacodes/Lavalink/releases/latest \
