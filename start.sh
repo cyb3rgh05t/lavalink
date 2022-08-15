8db40cd3ed08ea140e1f2741d0f90a58c4819594
cyb3rgh05t <flammang.yves@gmail.com>
2022-08-15 16:30:50 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,4 +16,4 @@ rm -f envsubst
 
 echo "All set! Now, let's start Lavalink!"
 
-exec java -Djdk.tls.client.protocols=TLSv1.1,TLSv1.2 -Xmx -jar Lavalink.jar
+exec java -Djdk.tls.client.protocols=TLSv1.1,TLSv1.2 -Xmx4G -jar Lavalink.jar
